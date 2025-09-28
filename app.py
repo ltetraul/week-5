757ac1f38cb1ac2f8124a780fb30a1d47504432c
ltetraul <143654006+ltetraul@users.noreply.github.com>
2025-09-28 14:27:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 from apputil import *
+from apputil import visualize_demographic
+from apputil import order_summary_table
+from apputil import visualize_families
+from apputil import visualize_age_division
 
 # Load Titanic dataset
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
@@ -12,16 +16,19 @@ st.write("Were children in third class more likely to survive than adult men in 
 '''
 )
 # Generate and display the figure
-fig1 = visualize_demographic()
+fig1 = visualize_demographic(order_summary_table(df))
 st.plotly_chart(fig1, use_container_width=True)
 
-st.write(
+last_name_counts = last_names(df)
+st.write("There are", last_name_counts.shape[0], "unique last names in the dataset.")
+         
+st.write("Which families had the most passengers on board and did larger families have higher survival rates?")
 '''
 # Titanic Visualization 2
 '''
-)
+
 # Generate and display the figure
-fig2 = visualize_families()
+fig2 = visualize_families(df, top_n=10)
 st.plotly_chart(fig2, use_container_width=True)
 
 st.write(
@@ -30,5 +37,5 @@ st.write(
 '''
 )
 # Generate and display the figure
-fig3 = visualize_family_size()
+fig3 = visualize_age_division(df)
 st.plotly_chart(fig3, use_container_width=True)
